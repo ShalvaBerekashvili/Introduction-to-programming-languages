403dc00c3b907616cbfa16bd63dfa95f30fe9321
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-25 10:41:16 +0300

    vebinar 25.02 Первая программа

---
+++ vebinar25.02/Program.cs
@@ -0,0 +1,4 @@
+﻿// Обявить тип переменной, обьявляем имя переммеенной, присваеваем 
+int inputNuber = Convert.ToInt32(Console.ReadLine());
+int square = Convert.ToInt32 (Math.Pow(inputNuber, 2));
+Console.WriteLine(square);
