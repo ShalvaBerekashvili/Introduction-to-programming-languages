b5682c82462ffa380d38889157b4025ea7b4d1a9
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-25 09:45:08 +0300

    add Example005 Condition If Else v.2

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
-﻿Cosole.Write("Введите имя пользователя"); // так и не заработало !()
+﻿Console.Write("Введите имя пользователя "); // так и не заработало !()
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же Маша!!!");
 }
