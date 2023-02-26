cb8d3541870adabb345604a0a4ab5ca6539f7384
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-26 11:53:58 +0300

    дано натуральное n. вычислить: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n.

---
+++ vebinar26.02/vebinar26.02.Zadacha#7/Program.cs
@@ -0,0 +1,9 @@
+﻿// дано натуральное n. вычислить: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n.
+
+int n = Convert.ToInt32(Console.RadLine());
+float sum = 0;
+for (int i = 1; i<=n; i++)
+{
+    sum += (float) 1 / (float) i;
+    System.Console.WriteLine((float) 1 / (float) i);
+}
