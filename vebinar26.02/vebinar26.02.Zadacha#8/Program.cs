e8305f87e48adc3db739e044ca2bb55da3beed37
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-26 11:55:41 +0300

    Нужно получить сумму от 2 в 0 до 2-х 10-й степени

---
+++ vebinar26.02/vebinar26.02.Zadacha#8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Нужно получить сумму от 2 в 0 до 2-х 10-й степени
+
+int n = 2;
+int sum = 0;
+for (int i = 0; i<=10; i++)
+{
+    sum += (int) Math.Pow(n, i);
+    
+}
+System.Console.WriteLine(sum);
