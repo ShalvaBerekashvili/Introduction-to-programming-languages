3ba7ba94c64f39279f949043103b8bd65c883a23
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-26 08:24:45 +0300

    Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго

---
+++ vebinar25.02.Zadacha#1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (b*b == a)
+{
+    System.Console.WriteLine("DA");
+}
+else
+{
+    System.Console.WriteLine("NET");
+}
