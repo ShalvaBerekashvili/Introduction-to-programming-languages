cdc318b887cf90d61ea0c2dd9272c78cb565aacb
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-26 10:34:28 +0300

    оторая выводит случайное трёхзначное число и удаляет вторую цифру этого числа

---
+++ vebinar26.02/vebinar26.02.Zadacha#2/Program.cs
@@ -0,0 +1,12 @@
+﻿// апишите программу, которая выводит случайное трёхзначное число и 
+//удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+int firstNumber = (number / 100) * 10;
+int lastNumber = number % 10;
+int result = firstNumber+lastNumber;
+System.Console.WriteLine(number);
+System.Console.WriteLine(result);
