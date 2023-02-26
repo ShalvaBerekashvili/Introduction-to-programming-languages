c7435879de606d3c9f4f728ddeb37e5ceda376db
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-02-26 10:22:45 +0300

    Напишите программу, которая выводит случайное число из отрезка [10, 99]

---
+++ vebinar26.02/vebinar26.02.Zadacha#1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Console.Clear();
+
+Random rnd = new Random(); 
+int number = rnd.Next(1, 100);
+
+System.Console.WriteLine(number);
