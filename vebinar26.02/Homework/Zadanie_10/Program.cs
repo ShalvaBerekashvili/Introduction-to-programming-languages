9eb1f6621097744d3aca4b6d43a0fc737c476231
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-04 08:32:15 +0300

    Zadanie_10 - на ввод трех чисел, и вывод значения второго числа

---
+++ vebinar26.02/Homework/Zadanie_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа
+
+System.Console.WriteLine("Enter a three-digit nuber: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = (a % 100) / 10;
+
+System.Console.Write("Result: ");
+System.Console.WriteLine(b);
