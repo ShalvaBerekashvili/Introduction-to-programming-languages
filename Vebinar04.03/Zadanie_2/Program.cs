e41db37d3b1ab556e269016b1a3458220a807acd
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-05 12:53:52 +0300

    Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).

---
+++ Vebinar04.03/Zadanie_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+System.Console.WriteLine("Введите число четверти: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Результат: ");
+if (a == 1)
+{
+     System.Console.WriteLine("x > 0, y > 0");
+}
+else if (a == 2)
+{
+    System.Console.WriteLine("x < 0, y > 0");
+}
+else if (a == 3)
+{
+    System.Console.WriteLine("x < 0, y < 0");
+}
+else if (a == 4)
+{
+    System.Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    System.Console.WriteLine("НЕ коректный ввод!");
+}
+
