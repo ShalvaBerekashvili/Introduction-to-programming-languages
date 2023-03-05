85386e018b34dcd6fdafd5102380d61be0b81ecb
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-05 15:39:39 +0300

    Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе

---
+++ Vebinar05.03/Zadanie_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.WriteLine("Ведите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int index = (int) Math.Log10(b) + 1;
+
+System.Console.Write(index);
