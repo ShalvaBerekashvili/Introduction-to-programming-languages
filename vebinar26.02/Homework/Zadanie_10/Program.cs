880674c729c343dee589377593cb2778bcde2961
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-05 09:32:29 +0300

    выводит третью цифру заданного числа или сообщает, что третьей цифры нет

--- vebinar26.02/Homework/Zadanie_10/Program.cs
+++ vebinar26.02/Homework/Zadanie_10/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
 // вторую цифру этого числа
 
-System.Console.WriteLine("Enter a three-digit nuber: ");
+System.Console.WriteLine("Enter a three-digit number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = (a % 100) / 10;
 
---
+++ vebinar26.02/Homework/Zadanie_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+System.Console.WriteLine("Эта программа выводит третью цифру!"); // приветствие программы!
+System.Console.WriteLine(" ");
+System.Console.WriteLine("Прошу введите число: ");
+System.Console.WriteLine(" ");
+
+int thirdТumber = Convert.ToInt32(Console.ReadLine());
+int index = (int) Math.Log10(thirdТumber) + 1; //Тут вычисляет сколько цифр в числе, и присватевает к index
+
+if (index < 3) // Проверяем наличие третьей цыфры.
+{
+    System.Console.Write("Третьей цифры нет! ");
+    System.Console.WriteLine("Ошибка!");
+}
+
+int enterNuber = thirdТumber.ToString()[2] - '0'; // Тут "отсекает" все лишнее и выадет цифру с индексом равное значению "a"
+
+System.Console.Write("Результат: ");
+System.Console.WriteLine(enterNuber);
