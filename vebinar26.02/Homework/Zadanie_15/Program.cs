476c6f72826f4e7037348fe7e2df177a388fbfee
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-05 09:59:01 +0300

    принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.

---
+++ vebinar26.02/Homework/Zadanie_15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+System.Console.WriteLine("Прошу введите, порядковый номер дня недели: ");
+int dailOfWeek = Convert.ToInt32(Console.ReadLine());
+ while (true) 
+{ 
+    if (dailOfWeek >= 1 && dailOfWeek <= 5) 
+        { 
+            Console.Write(" Нет, это не выходной");
+            break; 
+        } 
+        else if (dailOfWeek >= 6 && dailOfWeek <= 7) 
+        { 
+            Console.Write(" Да, это выходной");
+            break; 
+        } 
+        
+    else Console.Write(" Не существует такого дня недели");
+    break; 
+}
