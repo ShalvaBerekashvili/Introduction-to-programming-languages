a70d29d293d21de112f4dc4ecda3c597cff4ccba
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-19 15:29:48 +0300

    Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.

---
+++ Vebinar18.03/Zadacha_51/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+void FillArray2D(int [,] array2D)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+int SummDioganalArrya(int[,] array2D)
+{
+    int result = 0;
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result += array2D[i, j];;                
+            }
+        }
+    }
+    return result;
+}
+
+
+void PrintArray2D(int [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.Write(array2D[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int rows = 5;
+int colons = 7;
+int[,] array2D = new int[rows, colons];
+FillArray2D(array2D);
+PrintArray2D(array2D);
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.WriteLine(SummDioganalArrya(array2D));
+Console.ResetColor();
