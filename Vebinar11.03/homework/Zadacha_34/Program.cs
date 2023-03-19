662ccee504f30747760e3d274c8f1182662045b3
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-19 10:19:15 +0300

    Задайте массив заполненный случайными положительными трёхзначными числами Напишите программу, которая покажет количество чётных чисел в массиве

---
+++ Vebinar11.03/homework/Zadacha_34/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] EnterLengthArray(int size)
+        {
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(100, 1000);
+            }
+            return array;
+        }
+
+        void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                System.Console.Write($"{array[i],4}");
+            }
+        }
+
+        int FindEvenNumbersInAnArray(int[] evenNumbers)
+        {
+            int counter = 0;
+            for (int i = 0; i < evenNumbers.Length; i++)
+            {
+                if (evenNumbers[i] % 2 == 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        System.Console.WriteLine("Output of the filled array: ");
+        int[] array = EnterLengthArray(10);
+        PrintArray(array);
+        System.Console.WriteLine(" ");
+        System.Console.Write($"The number of even numbers in the array  {FindEvenNumbersInAnArray(array)}");
+    }
+}
