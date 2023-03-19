2dd1f15207f6088c43ff5d391d571ae3a4cb7546
Shalva Berekashvili <sbit_menedger@mail.ru>
2023-03-19 14:57:56 +0300

    Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива

---
+++ Vebinar19.03/Zadacha_53/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 53:Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void FillArray2D(int[,] array2D)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    array2D[i, j] = random.Next(1, 10);
+                }
+            }
+        }
+
+
+        void PrintArray2D(int[,] array2D)
+        {
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    System.Console.Write(array2D[i, j] + " ");
+                }
+
+                System.Console.WriteLine();
+            }
+        }
+
+        void SwapRowsInArray2D(int[,] array2D)
+        {
+            for (int j = 0; j < array2D.GetLength(1); j++)
+            {
+                int lastRowIndex = array2D.GetLength(0) - 1;
+                int firstRowIndex = 0;
+
+                (array2D[firstRowIndex, j], array2D[lastRowIndex, j]) = (array2D[lastRowIndex, j], array2D[firstRowIndex, j]);
+            }
+        }
+
+        int rows = 3;
+        int сolumns = 4;
+
+        int[,] array2D = new int[rows, сolumns];
+
+        FillArray2D(array2D);
+        PrintArray2D(array2D);
+        SwapRowsInArray2D(array2D);
+        System.Console.WriteLine();
+        PrintArray2D(array2D);
+    }
+}
